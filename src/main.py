75adce1d8c6e4bbff055714857bd8a3fe1721d67
isakrs <rathe.stoere@gmail.com>
2018-05-16 17:13:57 +0200

    Add main() now holds dist dict and orders.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from infrastructure import *
+from infrastructure import read_orders, Batch, Order, Pick, Warehouse
 from test_model import *
 
+
 ORDERS_FILE = "../data/DatenClient1.csv"
-DIST_FILE   = "../data/DistanceMatrix_Final.csv"
+DIST_FILE = "../data/DistanceMatrix_Final.csv"
 
 
 def main():
-    pass
+    dist = Warehouse().read_distances(DIST_FILE)
+    print("Size of the dist: ", len(dist))
 
+    orders = read_orders(ORDERS_FILE)
+    print("Number of orders: ", len(orders))
 
 if __name__ == '__main__':
     main()
