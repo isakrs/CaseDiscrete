f5d0551ba8793fdf01c3c8caabb81221eca717f6
isakrs <rathe.stoere@gmail.com>
2018-05-16 16:48:39 +0200

    Add main file to run model and infrastructure from.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from infrastructure import *
+from test_model import *
+
+ORDERS_FILE = "../data/DatenClient1.csv"
+DIST_FILE   = "../data/DistanceMatrix_Final.csv"
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
