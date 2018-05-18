c430928246b84a96f1069908b4bd663079da40c1
isakrs <rathe.stoere@gmail.com>
2018-05-18 10:20:01 +0200

    Add status on main.py.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from infrastructure import read_orders, Batch, Order, Pick, Warehouse
-from test_model import *
+from model import Model
+
+from datetime import datetime
 
 
-ORDERS_FILE = "../data/DatenClient1.csv"
+ORDERS_FILE = "../data/DatenClient1_day_1.csv"
 DIST_FILE = "../data/DistanceMatrix_Final.csv"
 
 
@@ -12,6 +14,17 @@ def main():
 
     orders = read_orders(ORDERS_FILE)
     print("Number of orders: ", len(orders))
+
+    start = datetime.now()
+    print('Model start time: ', str(start))
+
+    model = Model(orders, dist)
+
+    end = datetime.now()
+    print('Model duration time: ', str(end - start), 'Model ended: ', str(end))
+
+    print('number of used nodes: ', len(model._nodes))
+    print('number of variables: ', len(model._vars))
 
 if __name__ == '__main__':
     main()
