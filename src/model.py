861ecc0b4eebfb318003acb62e5f8e68a60f4a97
isakrs <rathe.stoere@gmail.com>
2018-06-18 11:27:39 +0200

    Save almost finished lazy constraint implementation. Some debugging left, but overall code structure is there.

--- src/model.py
+++ src/model.py
@@ -8,10 +8,13 @@ NAME_END_NODE = "F-20-27"
 
 def find_subsets(S):
     """Helper function which returns a set of all the subsets of set S.
+    
     Args:
         S (set or array): the set of which you want to output all the subsets.
+    
     Returns:
         set: returns a set of all subsets.
+    
     Example:
     >>> find_subsets(("1" ,"2" ,"3"))
     {'1', ('1', '3'), '3', ('1', '2'), (), ('2', '3'), '2'}
@@ -46,8 +49,8 @@ def subtourelim(model, where):
             print('key: ', key, 'content: ', content)
         print('nodes: ', model._nodes)
  
-        # for every batch make a list of nodes used in that batch
-        used_nodes = [[] for i in range(model._constants['max_n_batches'])]
+        # for every batch, make a list of edges
+        used_edges = [[] for i in range(model._constants['max_n_batches'])]
         for batch_k in range(model._constants['max_n_batches']):
             batch_k_vars = list()
             for i in range(len(model._nodes)-1):
@@ -60,29 +63,117 @@ def subtourelim(model, where):
             sol_batch_k = model.cbGetSolution(batch_k_vars)
             
             print('sol_batch_k: ', sol_batch_k)
+            
+            sol_batck_k_index = 0
+            for i in range(len(model._nodes)-1):
+                for j in range(i+1, len(model._nodes)):
+                    if sol_batch_k[sol_batck_k_index] > 0.5:
+                        used_edges[batch_k].append((model._nodes[i], model._nodes[j]))
+                    sol_batck_k_index += 1
 
-                              
-                #used_nodes[batch_k] += \
-                #    [ (self._nodes[i], self._nodes[j]) \
-                #      for j in range(len(self._nodes[i+1:])) \
-                #      if sol_batch_k[self._nodes[j]] > 0.5
-                #    ]
+        print('used_edges: ', used_edges)  
+
+        # for every batch, make a list of used nodes
+        used_nodes = [[] for i in range(model._constants['max_n_batches'])]
+        for batch_k in range(model._constants['max_n_batches']):
+            for node in model._nodes:
+                var = model._vars['B', batch_k, node]
+                if model.cbGetSolution(var) > 0.5: # then node is used
+                    used_nodes[batch_k].append(node)
+
+        # all necessary info is now attained in order to find subtours
+        # time to add constraints to prohibit the subtours
+        for batch_k in range(model._constants['max_n_batches']):
+            # function subtour finds the shortest cycle per batch
+            tour = _subtour(edges=used_edges[batch_k], n_nodes=len(used_nodes))
+            print('tour: ', tour)
+            print('tour type: ', type(tour))
+            """
+            if len(tour) < len(used_nodes[batch_k]):
+                # add a subtour elimination constraint
+                expr = 0
+                for i in range(len(tour)):
+                    for j in range(i+1, len(tour)):
+                        expr += model._vars['x', batch_k, tour[i], tour[j]]
+                model.cbLazy(expr <= len(tour)-1)
+            """
 
 
-    """                
-        for i in range(n):
-            sol = model.cbGetSolution([model._vars[i,j] for j in range(n)])
-            selected += [(i,j) for j in range(n) if sol[j] > 0.5]
-        # find the shortest cycle in the selected edge list
-        tour = subtour(selected)
-        if len(tour) < n:
-            # add a subtour elimination constraint
-            expr = 0
-            for i in range(len(tour)):
+            """                
+            # find the shortest cycle in the selected edge list
+            tour = subtour(selected)
+            if len(tour) < n:
+              # add a subtour elimination constraint
+              expr = 0
+              for i in range(len(tour)):
                 for j in range(i+1, len(tour)):
-                    expr += model._vars[tour[i], tour[j]]
-            model.cbLazy(expr <= len(tour)-1)
+                  expr += model._vars[tour[i], tour[j]]
+              model.cbLazy(expr <= len(tour)-1)
+            """
+
+def _subtour(edges, n_nodes):
+    """Given a list of edges, finds the shortest subtour
+
+    Args:
+        edges (:obj: `list`): list of edges. Each edge is written as a tuple (node_i, node_j)
+                              where node_i and node_j are strings, eg. ('F-20-28', 'F-20-27')
+               n_nodes (int): number of nodes the edges uses.
+
+    Returns:
+        shortest_cycle (:obj: `list`): all egdes that make up the shortest cycle. Is of the
+                                       same tuple format as egdes.
+    
+    Note: algorithm assumes the edges are tuples with int nodes, eg (node_i, node_j) is (1, 3)  
+        where node_i and node_j are in range(0,n_nodes). As a consequence, the edges are converted
+        using a look up dicts node_to_num and num_to_node 
     """
+    nodes = set()
+    for node_i, node_j in edges:
+        nodes.add(node_i)
+        nodes.add(node_j)
+
+    num_to_node = dict()
+    node_to_num = dict()
+    for i, node in enumerate(nodes):
+        num_to_node[i] = node
+        node_to_num[node] = i
+
+    edges_nums = list()
+    for node_i, node_j in edges:
+        edge_nums = node_to_num[node_i], node_to_num[node_j]
+        edges_nums.append(edge_nums)
+
+    # algorithm (uses edges_nums)
+    visited = [False]*n_nodes
+    cycles = []
+    lengths = []
+    selected = [[] for i in range(n_nodes)]
+    for x,y in edges_nums:
+        selected[x].append(y)
+    while True:
+        current = visited.index(False)
+        thiscycle = [current]
+        while True:
+            visited[current] = True
+            neighbors = [x for x in selected[current] if not visited[x]]
+            if len(neighbors) == 0:
+                break
+            current = neighbors[0]
+            thiscycle.append(current)
+        cycles.append(thiscycle)
+        lengths.append(len(thiscycle))
+        if sum(lengths) == n_nodes:
+            break
+        break
+    
+    shortest_cycle_nums = cycles[lengths.index(min(lengths))]
+    shortest_cycle = list()
+    for node_num_i, node_num_j in shortest_cycle_nums:
+        egde = num_to_node[node_num_i], num_to_node[node_num_j]
+        shortest_cycle.append(edge)
+
+    return shortest_cycle
+
 
 
 class Model(gp.Model):
@@ -131,9 +222,11 @@ class Model(gp.Model):
 
     def _used_nodes(self, orders):
         """Finds the used nodes and number of picks in the orders input.
+        
         Args:
             orders (:obj: `dict`): Dict of all orders.
                                    key: order_id (str) and item: (list of infrastructure.Order)
+        
         Returns:
              nodes (:obj: `list`): list of all node names (str)
                     n_picks (int): total number of picks
@@ -155,8 +248,10 @@ class Model(gp.Model):
 
     def _set_constants(self, orders, VOL=None, max_n_batches=None):
         """Sets all constant numbers for Model.
+        
         Note:
             Convention: superscripts are used before subscripts when indexing in dicts.
+        
         Args:
                     orders (:obj: `dict`): Dict of all orders.
                                            key: order_id (str) and item: (list of infrastructure.Order)
@@ -166,6 +261,7 @@ class Model(gp.Model):
             max_n_batches (int, optional): Maximum number of batches (tours in the warehouse) that is allowed. 
                                            If not given (default), ie None, then it will be set to 
                                            the total number of orders.
+        
         Returns:
              nodes (:obj: `dict`): dict with all Model constants
                                    key: constant name and item: value (float)
@@ -200,15 +296,18 @@ class Model(gp.Model):
 
     def _set_variables(self, dist, orders):
         """Initialise all the gurobi variables, and their objective function coefficients.
+        
         Note:
             Objective function coefficents, obj=, are also set when variables initialised.
             Default objective value is zero, and will remain so if not other is specified.
             Assuming an undirected graph, is i.e. x_i_j^k equals x_j_i^k (walking direction
             does not matter). Hence we only use x_i_j^k and not x_j_i^k.
+        
         Args:
               dist (:obj: `dict`): Dict with distances between nodes, eg dist['node_id_i']['node_id_j']
             orders (:obj: `dict`): Dict of all orders.
                                    key: order_id (str) and item: (list of infrastructure.Order)
+        
         Returns:
             _vars (:obj: `dict`): Dictionary of variables.
                                  key: id for variable, item: gurobi_model variable
